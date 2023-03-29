4e1337814e3651758f0a05242e4128c851951905
groundzerocash <128429194+groundzerocash@users.noreply.github.com>
2023-03-28 20:15:27 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ var imageThumbs = document.getElementById("image-thumbs");
 var currentImage = document.getElementById("current-image");
 for (var i = 1; i <= 9; i++){
     var thumb = document.createElement("img");
-    thumb.src = "image" + i + ".jpg";
-    thumb.alt = "Image " + i;
+    thumb.src = "image" + i + ".JPG";
+    thumb.alt = "image " + i;
     thumb.classList.add("thumb")
     imageThumbs.appendChild(thumb);
     thumb.addEventListener(
